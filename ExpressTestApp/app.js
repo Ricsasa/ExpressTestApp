4100a1e88870611182114ff2dcdfc18c429dfadd
Ricardo Sánchez Saldivar <ricsasa201@gmail.com>
2015-03-02 22:33:22 -0600

    Last changes

--- ExpressTestApp/app.js
+++ ExpressTestApp/app.js
@@ -5,6 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+//database
+var mongo = require('mongoskin');
+var db = mongo.db('mongodb://localhost:27017/nodete', {native_parser:true});
+
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -21,6 +26,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
